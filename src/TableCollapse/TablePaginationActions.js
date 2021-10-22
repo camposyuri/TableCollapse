80428e76578e1ab88689b75142a54068c21d90c8
Yuri Campos <45168468+camposyuri@users.noreply.github.com>
2021-10-22 11:39:12 -0300

    Fix

--- src/TableCollapse/TablePaginationActions.js
+++ src/TableCollapse/TablePaginationActions.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { IconButton } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
 
 const TablePaginationActions = ({ count, page, rowsPerPage, onPageChange }) => {
